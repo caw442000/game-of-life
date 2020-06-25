8e9e3d6d9de2611f8c5fb8c1c1991209d4b09a0c
cedric-winbush <cawinbushjr@gmail.com>
2020-06-24 19:40:05 -0500

    adds speed change, random, and size choice for board

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React, { useState, useCallback, useRef } from "react";
+import React, { useState, useCallback, useRef, useEffect } from "react";
 import produce from "immer";
 import "./App.css";
 
-const rowLength = 30;
-const columnLength = 30;
 const neighborLocations = [
   [0, 1],
   [0, -1],
@@ -14,22 +12,32 @@ const neighborLocations = [
   [1, 1],
   [-1, -1],
 ];
-const emptyBoard = () => {
-  const rows = [];
-  for (let i = 0; i < rowLength; i++) {
-    rows.push(Array.from(Array(columnLength), () => 0));
-  }
-  return rows;
-};
 
-const randomBoard = () => {
-  const rows = [];
-  for (let i = 0; i < rowLength; i++) {
-    rows.push(Array.from(Array(columnLength), () => Math.random() > 0.5 ? 1 : 0));
-  }
-  return rows;
-};
 function App() {
+  const [rowLength, setRowLength] = useState(30);
+  const [columnLength, setColumnLength] = useState(30);
+
+  const randomBoard = () => {
+    const rows = [];
+    for (let i = 0; i < rowLength; i++) {
+      rows.push(
+        Array.from(Array(columnLength), () => (Math.random() > 0.5 ? 1 : 0))
+      );
+    }
+    return rows;
+  };
+
+  const emptyBoard = (rowLength = 30, columnLength = 30) => {
+    setRowLength(rowLength);
+    setColumnLength(columnLength);
+    console.log("row", rowLength);
+    console.log("column", columnLength);
+    const rows = [];
+    for (let i = 0; i < rowLength; i++) {
+      rows.push(Array.from(Array(columnLength), () => 0));
+    }
+    return rows;
+  };
   const [board, setBoard] = useState(emptyBoard);
   const [buffer, setBuffer] = useState(emptyBoard);
   const [bufferTwo, setBufferTwo] = useState(emptyBoard);
@@ -37,9 +45,10 @@ function App() {
   const [running, setRunning] = useState(false);
   const [generations, setGenerations] = useState(0);
   const [changesMade, setChangesMade] = useState(false);
+  const [speed, setSpeed] = useState(1000);
 
   const handleClick = (i, j) => {
-    const newBoard = produce(board, boardCopy => {
+    const newBoard = produce(board, (boardCopy) => {
       boardCopy[i][j] = board[i][j] ? 0 : 1;
     });
     setBoard(newBoard);
@@ -51,75 +60,41 @@ function App() {
   };
   const runningRef = useRef(running);
   const generationsRef = useRef(generations);
-  const changesMadeRef = useRef(changesMade)
-  const bufferRef = useRef(buffer)
-  const bufferTwoRef = useRef(bufferTwo)
-  const currentBoardRef = useRef(currentBoard)
+  const changesMadeRef = useRef(changesMade);
+  const bufferRef = useRef(buffer);
+  const bufferTwoRef = useRef(bufferTwo);
+  const currentBoardRef = useRef(currentBoard);
+  const speedRef = useRef(speed);
   runningRef.current = running;
-  changesMadeRef.current = true
+  changesMadeRef.current = true;
+  const rowLengthRef = useRef(rowLength);
+  const columnLengthRef = useRef(columnLength);
 
-  const swap = (a, b) => {
-    const temp = a;
-    a = b;
-    b = temp;
-  }
-  const runGame = useCallback((buffer, bufferTwo) => {
+  rowLengthRef.current = rowLength;
+  columnLengthRef.current = columnLength;
+
+  // const updateBoard = (row, col) => {
+  //   rowLength = row
+  //   columnLength = col
+  //   emptyBoard(rowLength, columnLength)
+  //   console.log("row", rowLength)
+
+  // }
+
+  const runGame = useCallback((currentStatus, nextStatus) => {
     if (!runningRef.current) {
       return;
     }
-      generationsRef.current += 1;
 
     changesMadeRef.current = false;
 
-    currentBoardRef.current = board;
+    let current = currentStatus;
+    let next = nextStatus;
 
-
-    // setBoard((board) => {
-      
-    //   return produce(board, (newBoard) => {
-    //     for (let i = 0; i < rowLength; i++) {
-    //       for (let j = 0; j < columnLength; j++) {
-    //         let neighbors = 0;
-    //         // if(newBoard[i][j + 1] === 1){
-    //         //   neighbors += 1
-    //         // }
-    //         neighborLocations.forEach(([x, y]) => {
-    //           const iCheck = i + x;
-    //           const jCheck = j + y;
-    //           if (
-    //             iCheck >= 0 &&
-    //             iCheck < rowLength &&
-    //             jCheck >= 0 &&
-    //             jCheck < columnLength
-    //           ) {
-    //             neighbors += board[iCheck][jCheck];
-    //           }
-    //         });
-    //         if (neighbors < 2 || neighbors > 3) {
-    //           newBoard[i][j] = 0;
-    //           console.log("change made 1 ", changesMadeRef.current)
-    //         } else if (board[i][j] === 0 && neighbors === 3) {
-    //           newBoard[i][j] = 1;
-    //           changesMadeRef.current = true;
-
-    //           console.log("change made 2 ", changesMadeRef.current)
-
-    //         }
-    //       }
-    //     }
-    //     console.log("changes made", changesMadeRef.current)
-    //     if(changesMadeRef.current === false){
-    //       runningRef.current = !running;
-    //       return
-    //     }
-    //   });
-    // });
-
-
-    bufferRef.current = (board) => {
-      return produce(board, (newBoard) => {
-        for (let i = 0; i < rowLength; i++) {
-          for (let j = 0; j < columnLength; j++) {
+    let buffer = (current) => {
+      return produce(current, (newBoard) => {
+        for (let i = 0; i < rowLengthRef.current; i++) {
+          for (let j = 0; j < columnLengthRef.current; j++) {
             let neighbors = 0;
             // if(newBoard[i][j + 1] === 1){
             //   neighbors += 1
@@ -129,39 +104,85 @@ function App() {
               const jCheck = j + y;
               if (
                 iCheck >= 0 &&
-                iCheck < rowLength &&
+                iCheck < rowLengthRef.current &&
                 jCheck >= 0 &&
-                jCheck < columnLength
+                jCheck < columnLengthRef.current
               ) {
-                neighbors += board[iCheck][jCheck];
+                neighbors += current[iCheck][jCheck];
               }
             });
             if (neighbors < 2 || neighbors > 3) {
               newBoard[i][j] = 0;
-              console.log("change made 1 ", changesMadeRef.current)
             } else if (board[i][j] === 0 && neighbors === 3) {
               newBoard[i][j] = 1;
               changesMadeRef.current = true;
-
-              console.log("change made 2 ", changesMadeRef.current)
-
             }
           }
         }
-        console.log("changes made", changesMadeRef.current)
-        if(changesMadeRef.current === false){
+        if (changesMadeRef.current === false) {
           runningRef.current = !running;
-          return
+          return;
         }
       });
     };
 
+    let bufferTwo = (next) => {
+      return produce(next, (newBufferTwo) => {
+        for (let i = 0; i < rowLengthRef.current; i++) {
+          for (let j = 0; j < columnLengthRef.current; j++) {
+            let neighbors = 0;
+            // if(newBoard[i][j + 1] === 1){
+            //   neighbors += 1
+            // }
+            neighborLocations.forEach(([x, y]) => {
+              const iCheck = i + x;
+              const jCheck = j + y;
+              if (
+                iCheck >= 0 &&
+                iCheck < rowLengthRef.current &&
+                jCheck >= 0 &&
+                jCheck < columnLengthRef.current
+              ) {
+                neighbors += next[iCheck][jCheck];
+              }
+            });
+            if (neighbors < 2 || neighbors > 3) {
+              newBufferTwo[i][j] = 0;
+            } else if (next[i][j] === 0 && neighbors === 3) {
+              newBufferTwo[i][j] = 1;
+              changesMadeRef.current = true;
+            }
+          }
+        }
+        if (changesMadeRef.current === false) {
+          runningRef.current = !running;
+          return;
+        }
+      });
+    };
 
-    setBoard(bufferRef.current)
+    next = bufferTwo;
 
-    
+    // display = bufferRef.current
 
-    setTimeout(runGame, 500, buffer, bufferTwo);
+    generationsRef.current += 1;
+    console.log("generation", generationsRef.current);
+
+    setBoard(current);
+
+    // when board is set make next grid current and pass next grid down
+
+    // const temp =
+
+    // bufferRef.current = bufferTwoRef.current
+
+    let temp = current;
+    current = next;
+    next = current;
+
+    console.log("speed", speedRef.current);
+
+    setTimeout(runGame, speedRef.current, current);
   }, []);
 
   // console.log("total", board.reduce((a,b) => a+ b,0));
@@ -172,7 +193,8 @@ function App() {
           setRunning(!running);
           if (!running) {
             runningRef.current = true;
-            runGame();
+            // generationsRef.current += 1;
+            runGame(board);
           }
         }}
       >
@@ -180,8 +202,11 @@ function App() {
       </button>
       <button
         onClick={() => {
-          setBoard(emptyBoard);
-          setRunning(!running);
+          setBoard(emptyBoard(rowLength, columnLength));
+          if (running) {
+            setRunning(!running);
+          }
+
           generationsRef.current = 0;
         }}
       >
@@ -189,12 +214,70 @@ function App() {
       </button>
       <button
         onClick={() => {
-          setBoard(randomBoard); 
+          setBoard(randomBoard(rowLength, columnLength));
+          generationsRef.current = 0;
+
         }}
       >
         random
       </button>
+      <button
+        onClick={() => {
+          speedRef.current *= 2;
+          setSpeed(speedRef.current)
+        }}
+      >
+        slower
+      </button>
+      <button
+        onClick={() => {
+          if (speedRef.current >= 50) {
+            speedRef.current /= 2;
+            setSpeed(speedRef.current)
+          }
+        }}
+      >
+        fast
+      </button>
+      <button
+        onClick={() => {
+          setBoard(emptyBoard(30, 30));
+          if (running) {
+            setRunning(!running);
+          }
+
+          generationsRef.current = 0;
+        }}
+      >
+        small
+      </button>
+      <button
+        onClick={() => {
+          setBoard(emptyBoard(50, 50));
+          if (running) {
+            setRunning(!running);
+          }
+
+          generationsRef.current = 0;
+        }}
+      >
+        medium
+      </button>
+      <button
+        onClick={() => {
+          setBoard(emptyBoard(75, 75));
+          if (running) {
+            setRunning(!running);
+          }
+
+          generationsRef.current = 0;
+        }}
+      >
+        large
+      </button>
+
       <h1> Generations: {generationsRef.current} </h1>
+      <h1> Speed: {speed / 1000} sec </h1>
 
       <div
         className="App"
